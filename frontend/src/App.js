a68463196f76e0c7e11a1f80f8754f4ea6600663
wgmin <min20000416@naver.com>
2023-11-19 12:35:33 +0900

    #Feat: Frontend 라우터 및 컴포넌트 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import Main from "./domain/Main/Main";
 import Team from "./domain/Team/Team";
 import NotFound from "./components/Error/NotFound";
